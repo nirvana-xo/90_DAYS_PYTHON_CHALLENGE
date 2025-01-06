6d20b53d8ea86470a0ff5e0ff3550f658d924481
Richard Nti <rnti5@st.knust.edu.gh>
2025-01-06 09:23:10 +0000

    Day 1 of 90 days python challenge

---
+++ day1.py
@@ -0,0 +1,9 @@
+#Day 1: Python Introduction and Setup
+#Write a simple Python script that prints "Hello, World!" and the results of basic arithmetic (e.g., 2 + 2, 5 * 3).
+
+print("Hello, World!")
+print(2 + 3)
+print(5 * 3)
+print(10 / 2)
+print(10 - 2)
+print(10 == 2)
