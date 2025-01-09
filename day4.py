f42db9c40ab8c2b8635939bb299cdb5d23fe9437
Nirvana <rnti5@st.knust.edu.gh>
2025-01-09 13:17:03 -0800

    Day 4 of 90days of python challenge

---
+++ day4.py
@@ -0,0 +1,14 @@
+"""
+Day 4: Loops
+Topics:
+ - Learn about for and while loops.
+ - Iterating through lists and dictionaries.
+Project:
+ - Create a simple "countdown" program that 
+    counts down from 10 to 1 using a while loop.
+"""
+
+i = 10
+while i != 0:
+    print(i)
+    i-=1
